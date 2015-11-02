82b2c67ec0ac7a50bbd48ba470bf01520e49b565
Toby Smyth <tobs169@gmail.com>
2015-11-02 18:00:37 +0000

    Pretty print

--- src/HulkImport.hs
+++ src/HulkImport.hs
@@ -6,18 +6,22 @@ where
 import Lib
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T 
+import qualified Text.PrettyPrint as P
 
 importFile :: FilePath -> FilePath  -> IO ()
 importFile input output = do
   contents <- TIO.readFile input
-  TIO.writeFile output $ toSQL $ parseFile contents
+  TIO.writeFile output $ T.pack $ P.render $ toSQL $ parseFile contents
 
 
-toSQL :: CSV -> T.Text
-toSQL (CSV rows) = T.pack $ "VALUES " ++ (rows >>= toValues) ++ "\nGO"
+toSQL :: CSV -> P.Doc
+toSQL (CSV rows) = P.hcat (P.text "VALUES " : fmap ((P.<> P.text ", ")  . toValues) rows)
+                   P.<> P.text "\nGO"
 
 
-toValues :: [T.Text] -> String
-toValues elems = "(" ++ 
-                 (T.unpack . T.intercalate (T.pack ",")) elems
-                 ++ "), "
+toValues :: [T.Text] -> P.Doc
+toValues  =
+    P.parens
+         . P.hcat
+         . P.punctuate (P.text ",")
+         . map (P.text . T.unpack)

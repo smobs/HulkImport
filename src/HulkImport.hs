0e3758df6303fe3aff6728b8aec7cd23ce1c342b
Toby Smyth <tobs169@gmail.com>
2015-11-02 07:47:46 +0000

    Pass gold test

--- src/HulkImport.hs
+++ src/HulkImport.hs
@@ -10,6 +10,14 @@ import qualified Data.Text as T
 importFile :: FilePath -> FilePath  -> IO ()
 importFile input output = do
   contents <- TIO.readFile input
-  TIO.writeFile output $ T.pack $ show $ parseFile contents
+  TIO.writeFile output $ toSQL $ parseFile contents
 
 
+toSQL :: CSV -> T.Text
+toSQL (CSV rows) = T.pack $ "VALUES " ++ (rows >>= toValues) ++ "\nGO"
+
+
+toValues :: [T.Text] -> String
+toValues elems = "(" ++ 
+                 (T.unpack . T.intercalate (T.pack ",")) elems
+                 ++ "), "

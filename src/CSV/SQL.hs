6a19dfcf893fe9df169d12e2c2cabc2505adfa66
Toby Smyth <tobs169@gmail.com>
2015-11-03 07:35:55 +0000

    Simplify

--- src/CSV/SQL.hs
+++ src/CSV/SQL.hs
@@ -7,14 +7,15 @@ import Text.PrettyPrint
 import qualified Data.Text as T
 
 toSQL :: CSV -> String
-toSQL (CSV rows) = render $ 
-                   hcat (text "VALUES " : fmap ((<> text ", ")  . toValues) rows)
+toSQL  = render . csvDoc
+
+csvDoc :: CSV -> Doc
+csvDoc (CSV rows) = hcat (text "VALUES " : fmap toValues rows)
                    <> text "\nGO"
-
-
 
 toValues :: [T.Text] -> Doc
 toValues  =
+    (<> text ", ") .
     parens
          . hcat
          . punctuate (text ",")

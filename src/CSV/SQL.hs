a9bd14a97f03d64433c54f86fcae72f9e2f25a82
Toby Smyth <tobs169@gmail.com>
2015-11-03 07:29:43 +0000

    dont qualify

--- src/CSV/SQL.hs
+++ src/CSV/SQL.hs
@@ -3,20 +3,20 @@ module CSV.SQL
 where
 
 import CSV.Parse
-import qualified Text.PrettyPrint as P
+import Text.PrettyPrint
 import qualified Data.Text as T
 
 toSQL :: CSV -> String
-toSQL (CSV rows) = P.render $ 
-                   P.hcat (P.text "VALUES " : fmap ((P.<> P.text ", ")  . toValues) rows)
-                   P.<> P.text "\nGO"
+toSQL (CSV rows) = render $ 
+                   hcat (text "VALUES " : fmap ((<> text ", ")  . toValues) rows)
+                   <> text "\nGO"
 
 
 
-toValues :: [T.Text] -> P.Doc
+toValues :: [T.Text] -> Doc
 toValues  =
-    P.parens
-         . P.hcat
-         . P.punctuate (P.text ",")
-         . map (P.text . T.unpack)
+    parens
+         . hcat
+         . punctuate (text ",")
+         . map (text . T.unpack)
 

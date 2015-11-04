0be1c9ef62548b3cb2c707def63b786d9e11b9bf
Toby Smyth <tobs169@gmail.com>
2015-11-04 08:59:21 +0000

    default options

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,21 @@ module Main where
 
 import HulkImport
 
+data Options = Options {
+      optInput :: FilePath,
+      optOutput :: FilePath
+    }
+
 main :: IO ()
-main = importFile testFile "./output"
+main = do
+  opt <- defaultOptions
+  importFile (optInput opt) (optOutput opt)
 
 testFile :: FilePath
 testFile = "/Users/toby/Downloads/Countries_v1.csv"
+
+defaultOptions :: IO Options
+defaultOptions = Options 
+                 <$> (putStrLn "Please provide an input file:" >> getLine) 
+                 <*> return "./output"
+

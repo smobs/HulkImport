5470384d02ffe94d4c6a94738e08f5eee49dae2b
Toby Smyth <tobs169@gmail.com>
2015-11-04 09:36:55 +0000

    Command line args

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import System.Environment
+import System.Console.GetOpt
+
 import HulkImport
 
 data Options = Options {
@@ -9,14 +12,27 @@ data Options = Options {
 
 main :: IO ()
 main = do
-  opt <- defaultOptions
+  args <- getArgs
+  let opt = buildOptions options args defaultOptions
   importFile (optInput opt) (optOutput opt)
 
 testFile :: FilePath
 testFile = "/Users/toby/Downloads/Countries_v1.csv"
 
-defaultOptions :: IO Options
-defaultOptions = Options 
-                 <$> (putStrLn "Please provide an input file:" >> getLine) 
-                 <*> return "./output"
+defaultOptions :: Options
+defaultOptions = Options "./input"  "./output"
 
+
+buildOptions :: [OptDescr (Options -> Options)] -> [String] -> Options -> Options
+buildOptions o =
+    let f (x, _, _) = x in
+    applyOptions . f  . getOpt Permute o
+
+applyOptions :: [Options -> Options] -> Options -> Options
+applyOptions fs o = foldr id o fs
+
+options :: [OptDescr (Options -> Options)]
+options = [ Option ['i'] ["input"]
+                       (ReqArg (\f opt -> opt {optInput = f}) "FILE") "input file"
+          , Option ['o'] ["output"]
+                (ReqArg (\f opt -> opt {optOutput = f}) "FILE") "output file" ]

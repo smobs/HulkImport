5f54347ede9cbecd4f6ac4eba1fe1e8fb595dab1
unknown <toby.smyth@TST-TOBY.red-gate.com>
2015-11-11 10:27:06 +0000

    stylish

--- src/CSV/Parse.hs
+++ src/CSV/Parse.hs
@@ -3,15 +3,15 @@ module CSV.Parse
     ( parse
     ) where
 
+import           Control.Applicative
 import qualified Data.Attoparsec.Text as A
-import qualified Data.Text as Text
-import Control.Applicative
+import qualified Data.Text            as Text
 
-import CSV.Types
+import           CSV.Types
 
 -- | 'parse' parses text in the CSV format.  Delimiters are ',' and new lines.
 -- Quotation is performed with '"'.
--- TODO: Files in the wrong format throw an error :(   
+-- TODO: Files in the wrong format throw an error :(
 parse :: Text.Text -> CSV SQLVal
 parse text =
     case A.parseOnly csvParser text of
@@ -20,12 +20,12 @@ parse text =
 
 csvParser :: A.Parser (CSV SQLVal)
 csvParser = do
-  rows <- A.sepBy1 rowParser (A.skip A.isEndOfLine) 
+  rows <- A.sepBy1 rowParser (A.skip A.isEndOfLine)
   return (CSV rows)
 
 
 rowParser :: A.Parser [SQLVal]
-rowParser = A.sepBy1 elementParser $ A.string ", "
+rowParser = A.sepBy1 elementParser $ A.string ","
 
 elementParser :: A.Parser SQLVal
 elementParser =
@@ -40,10 +40,10 @@ textParser :: A.Parser Text.Text
 textParser = A.takeWhile1 (A.notInClass [',','\n', '"'])
 
 quotationParser :: A.Parser Text.Text
-quotationParser = 
+quotationParser =
     let quote = '"' in
     let f q1 t q2 = Text.cons q1 (Text.snoc t q2) in
-    f <$> A.char quote <*> 
+    f <$> A.char quote <*>
     quotedString [quote] <*>
     A.char quote
 
